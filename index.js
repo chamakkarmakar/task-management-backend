3e78df16bf529f31c182f0a5f2140d0d1a9e5192
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-06-14 01:23:28 +0600

    added task to the DB

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+// xUq4fOH7kMHQQiBs 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q0axwt3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const tasksDB = client.db("tasksDB");
+        const tasksCollection = tasksDB.collection("tasksCollection");
+
+       
+
+        // create data 
+        app.post("/allTasks", async (req, res) => {
+            const tasksData = req.body;
+            const result = await tasksCollection.insertOne(tasksData);
+            res.send(result);
+        });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("Task Management is working");
